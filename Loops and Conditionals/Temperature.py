acee3bd4e2c083fab59d83313933bf7cce5face6
Nishant Gautam <eddienishant@gmail.com>
2020-06-24 21:50:23 +0530

    This

---
+++ Loops and Conditionals/Temperature.py
@@ -0,0 +1,15 @@
+"""
+
+Convert the temperature from Fahrenheit to Celsius in the function below. You can use this formula:
+
+C^\circ = (F^\circ - 32)\times \frac{5}{9}C
+∘
+ =(F
+∘
+ −32)×
+9
+5
+​
+
+Round the returned result to 3 decimal places.
+"""
